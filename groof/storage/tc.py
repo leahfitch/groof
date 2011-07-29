ed95cadb6a632b8ae8c1fc410529f1e87fbb9dd4
Elisha Cook <elisha@elishacook.com>
2011-07-29 09:45:02 -0400

    Updated license and copyright information and added a README

--- groof/storage/tc.py
+++ groof/storage/tc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009 Elisha Cook <ecook@justastudio.com>
+# Copyright (c) 2009 Elisha Cook <elisha@elishacook.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
